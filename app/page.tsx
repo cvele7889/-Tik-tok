1baf20d8121d4ecea58aa8289473a5f7542a9943
Cvele <marko3ecvetkovic@gmail.com>
2025-01-20 14:28:55 +0100

    Prvi veci komit

---
+++ app/page.tsx
@@ -0,0 +1,14 @@
+"use client"
+import MainLayout from "./layouts/MainLayout";
+
+export default function Home() {
+  return (
+    <>
+    <MainLayout>
+      <div>
+        HOME
+      </div>
+    </MainLayout>
+    </>
+  );
+}
---
+++ next.config.ts
@@ -0,0 +1,11 @@
+import type { NextConfig } from "next";
+
+const nextConfig: NextConfig = {
+  webpack:(config, {isServer})=>{
+    config.module.rules.push({test:/\.node$/,use: 'raw-loader'})
+if(!isServer) config.externals.push('canvas');
+return config;
+  }
+};
+
+export default nextConfig;
