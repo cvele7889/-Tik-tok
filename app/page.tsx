45476acd895f3821164a34c6110a597819bb4101
Cvele <marko3ecvetkovic@gmail.com>
2025-01-21 13:31:47 +0100

    Nav and Main page

--- app/page.tsx
+++ app/page.tsx
@@ -1,14 +1,31 @@
-"use client"
+"use client";
+import ClientOnly from "./components/ClientOnly";
 import MainLayout from "./layouts/MainLayout";
+import PostMain from "./components/PostMain";
 
 export default function Home() {
   return (
     <>
-    <MainLayout>
-      <div>
-        HOME
-      </div>
-    </MainLayout>
+      <MainLayout>
+        <div className="mt-[80px] w-[calc(100%-90px)] max-w-[690px] ml-auto">
+          <ClientOnly>
+            <PostMain
+              post={{
+                id: "123",
+                user_id: "456",
+                video_url: "/sea.mp4",
+                text: "this is some text",
+                created_at: "date here",
+                profile: {
+                  user_id: "456",
+                  name: "User 1",
+                  image: "https://placehold.co/100",
+                },
+              }}
+            />
+          </ClientOnly>
+        </div>
+      </MainLayout>
     </>
   );
 }
