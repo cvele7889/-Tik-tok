5e250db70563884545ea213652529001839e61e8
Cvele <marko3ecvetkovic@gmail.com>
2025-01-22 16:09:58 +0100

    Profile page

--- app/types.tsx
+++ app/types.tsx
@@ -3,10 +3,27 @@ export interface RandomUsers {
   name: string;
   image: string;
 }
+export interface CropperDimensions {
+  height?: number | null;
+  width?: number | null;
+  left?: number | null;
+  top?: number | null;
+}
+export interface ShowErrorObject {
+  type: string;
+  message: string;
+}
 export interface Like {
   id: string;
   user_id: string;
   post_id: string;
+}
+export interface Post {
+  id: string;
+  user_id: string;
+  video_url: string;
+  text: string;
+  created_at: string;
 }
 export interface Comment {
   id: string;
@@ -34,8 +51,14 @@ export interface UploadError {
 export interface PostMainLikesCompTypes {
   post: PostWithProfile;
 }
+export interface PostUserCompTypes {
+  post: Post;
+}
 export interface PostMainCompTypes {
   post: PostWithProfile;
+}
+export interface ProfilePageTypes {
+  params: { id: string };
 }
 export interface MenuItemsTypes {
   iconString: string;
@@ -44,4 +67,11 @@ export interface MenuItemsTypes {
 }
 export interface MenuItemFollowCompTypes {
   user: RandomUsers;
+}
+export interface TextInputCompTypes {
+  string: string;
+  inputType: string;
+  placeholder: string;
+  onUpdate: (newValue: string) => void;
+  error: string;
 }
